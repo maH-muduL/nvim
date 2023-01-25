1680c832f2d9c54b0f5a83515dc2fb484d45e1cd
Mahmudul Hasan <94689315+maH-muduL@users.noreply.github.com>
2023-01-26 00:56:41 +0600

    Create packer_init.lua

---
+++ lua/packer_init.lua
@@ -0,0 +1,67 @@
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- Install plugins
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use "rebelot/kanagawa.nvim"
+  use {'catppuccin/nvim', as = 'catppuccin'}
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+  use {'nvim-lualine/lualine.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+  use {"akinsho/toggleterm.nvim", tag = '*'}
+  use {'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = 'nvim-lua/plenary.nvim'}
+  use 'ervandew/supertab'
+  use 'windwp/nvim-autopairs'
+  use 'numToStr/Comment.nvim'
+  use 'christoomey/vim-tmux-navigator'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+-- LSP plugins
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+
+-- For luasnip users.
+  use 'L3MON4D3/LuaSnip'
+  use "rafamadriz/friendly-snippets"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

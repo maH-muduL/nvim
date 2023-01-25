49cee3807ea51f457d459adc3287d2c9f1f4c1b9
Mahmudul Hasan <94689315+maH-muduL@users.noreply.github.com>
2023-01-26 01:08:02 +0600

    Create tokyonight.lua

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,14 @@
+require("tokyonight").setup({
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    functions = {}
+  },
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.error = "#ff0000"
+  end
+})
